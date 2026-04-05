59f78397cc1c5d16aa8ba7b67db1523791829784
Jennifer Weir <contact@jenniferpweir.com>
2026-04-04 21:37:37 -0400

    fix: revert storage path change

--- api/common/database.js
+++ api/common/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: '/api/storage/data.db'
+  storage: './storage/data.db'
 });
 module.exports = sequelize;
